9b731160437f7984f52df53ac22201f7b9613ad5
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-01-30 00:27:50 +0300

    Add project files.

---
+++ HackerRank/Classes/Result.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.simpleArraySum
+{
+    public class Result
+    {
+        /*
+            * Complete the 'miniMaxSum' function below.
+            *
+            * The function accepts INTEGER_ARRAY arr as parameter.
+            */
+
+        public static void miniMaxSum(List<int> arr)
+        {
+            long total = 0;
+
+            List<long> results = new List<long>();
+            List<long> results2 = new List<long>();
+
+            foreach (var item in arr)
+            {
+                total += item;
+            }
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                long res = total - arr[i];
+                results.Add(res);
+            }
+
+            long minResult = results.Min();
+            long maxResult = results.Max();
+
+            Console.WriteLine(minResult + " " + maxResult);
+        }
+    }
+}
---
+++ HackerRank/Classes/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.simpleArraySum
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            List<int> arr = new List<int>();
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(4);
+            arr.Add(5);
+
+
+            Result.miniMaxSum(arr);
+
+        }
+    }
+}
