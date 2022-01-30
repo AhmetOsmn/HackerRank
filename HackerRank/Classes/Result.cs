7821ecf5f17d9d357dcab80a712de0f3328f8267
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-01-31 02:19:29 +0300

    Grading Students

--- HackerRank/Classes/Result.cs
+++ HackerRank/Classes/Result.cs
@@ -8,6 +8,7 @@ namespace HackerRank.simpleArraySum
 {
     public class Result
     {
-        
+       
+
     }
 }
--- HackerRank/Classes/Solution.cs
+++ HackerRank/Classes/Solution.cs
@@ -11,6 +11,23 @@ namespace HackerRank.simpleArraySum
 
         public static void Main(string[] args)
         {
+            /*
+             * Complete the 'countApplesAndOranges' function below.
+             *
+             * The function accepts following parameters:
+             *  1. INTEGER s
+             *  2. INTEGER t
+             *  3. INTEGER a
+             *  4. INTEGER b
+             *  5. INTEGER_ARRAY apples
+             *  6. INTEGER_ARRAY oranges
+             */
+
+            public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+            {
+
+            }
+
 
         }
 
