03fdf2105a71d1d6d43d6855a39ca1b968704cf4
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-07-20 16:47:36 +0300

    5. problem eklendi

---
+++ ProjectEuler/Solutions/Solutions/Problem5.cs
@@ -0,0 +1,37 @@
+﻿namespace Solutions
+{
+    public class Problem5
+    {
+        /*
+         * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+         * 
+         * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+         */
+
+        public static void Solution()
+        {
+            int result = 2520;
+         
+            while(!IsDivisible(result))
+            {
+                result += 2520;
+            }
+
+            Console.WriteLine("result: " + result);
+        }
+
+        public static bool IsDivisible(int num)
+        {
+            // int[] factors = {2,3,5,7,9};
+            int[] factors = {11,12,13,14,15,16,17,18,19,20};
+            
+            foreach (int item in factors)
+            {
+                if(num % item != 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- ProjectEuler/Solutions/Solutions/Program.cs
+++ ProjectEuler/Solutions/Solutions/Program.cs
@@ -9,7 +9,8 @@ namespace Solutions
             // Problem1.Solution();
             // Problem2.Solution();
             // Problem3.Solution();
-            Problem4.Solution();
+            // Problem4.Solution();
+            Problem5.Solution();
         }
     }
 }
